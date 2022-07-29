fae6ffe0965a9b097f39ff28adfedb1a0e306e12
zikriaulia28 <zikriaulia98@gmail.com>
2022-07-29 18:41:48 +0700

    fix all

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
-document.querySelector('.menu-toggle').addEventListener('click', function () {
-    const cls = [...this.classList];
-    this.classList.remove(cls.includes('bxs-grid-alt') ? 'bxs-grid-alt' : 'bx-x');
-    this.classList.add(cls.includes('bxs-grid-alt') ? 'bx-x' : 'bxs-grid-alt');
-  })
+const menuToggle = document.querySelector('.menu-toogle input');
+const nav = document.querySelector('.nav-items');
+
+	menuToggle.addEventListener('click', function () {
+		nav.classList.toggle('active')
+	});
+
+
+  let anchor = document.querySelectorAll('.nav-items li a')
+	for( let i = 0; i < anchor.length; i++ ){
+		anchor[i].addEventListener('click', function(){
+
+		let active = nav.classList.contains('active')
+			if(active){	
+				nav.classList.remove('active')
+			}
+		  })
+     	}
+
