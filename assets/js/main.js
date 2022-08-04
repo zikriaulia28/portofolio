32c89b4902b94f375e44d891c65d530175f3c6d8
zikriaulia28 <zikriaulia98@gmail.com>
2022-08-04 18:53:25 +0700

    add JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,35 @@
-const menuToggle = document.querySelector('.menu-toogle input');
-const nav = document.querySelector('.nav-items');
+const menuToggle = document.querySelector(".menu-toogle input");
+const nav = document.querySelector(".nav-items");
+let icon = document.querySelector(".fa-moon");
+let btn = document.querySelector(".btn-dark");
+let h1 = document.querySelector(".heading");
+let sub_heading = document.querySelector(".sub-heading");
+let text_fe = document.querySelector(".text-fe");
 
-	menuToggle.addEventListener('click', function () {
-		nav.classList.toggle('active')
-	});
+// dark mode
+btn.onclick = function () {
+  document.body.classList.toggle("dark-body");
+  h1.classList.toggle("dark-body");
+  sub_heading.classList.toggle("dark-body");
+  text_fe.classList.toggle("dark-body");
+  if (document.body.classList.contains("dark-body")) {
+    icon.classList.replace("fa-moon", "fa-sun");
+  } else {
+    icon.classList.replace("fa-sun", "fa-moon");
+  }
+};
 
+// hamburger
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("active");
+});
 
-  let anchor = document.querySelectorAll('.nav-items li a')
-	for( let i = 0; i < anchor.length; i++ ){
-		anchor[i].addEventListener('click', function(){
-
-		let active = nav.classList.contains('active')
-			if(active){	
-				nav.classList.remove('active')
-			}
-		  })
-     	}
-
+let anchor = document.querySelectorAll(".nav-items li a");
+for (let i = 0; i < anchor.length; i++) {
+  anchor[i].addEventListener("click", function () {
+    let active = nav.classList.contains("active");
+    if (active) {
+      nav.classList.remove("active");
+    }
+  });
+}
