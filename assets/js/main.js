c4d9e083b92d28e3b8990aecbe2e4df6e3379f58
zikriaulia28 <zikriaulia98@gmail.com>
2022-08-04 21:34:08 +0700

    add JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,9 @@ let btn = document.querySelector(".btn-dark");
 let h1 = document.querySelector(".heading");
 let sub_heading = document.querySelector(".sub-heading");
 let text_fe = document.querySelector(".text-fe");
+let heading_skills = document.querySelector(".heading-skills");
+let heading_contact = document.querySelector(".heading-contact");
+let text_contact = document.querySelector(".text-contact");
 
 // dark mode
 btn.onclick = function () {
@@ -12,6 +15,9 @@ btn.onclick = function () {
   h1.classList.toggle("dark-body");
   sub_heading.classList.toggle("dark-body");
   text_fe.classList.toggle("dark-body");
+  heading_skills.classList.toggle("dark-body");
+  heading_contact.classList.toggle("dark-body");
+  text_contact.classList.toggle("dark-body");
   if (document.body.classList.contains("dark-body")) {
     icon.classList.replace("fa-moon", "fa-sun");
   } else {
