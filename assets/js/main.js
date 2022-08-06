d3e05cdc429288528c5f1f051bddfe3a63bd7e18
zikriaulia28 <zikriaulia98@gmail.com>
2022-08-06 13:06:16 +0700

    fix all

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,22 @@
-const menuToggle = document.querySelector(".menu-toogle input");
+const menuToggle = document.querySelector(".menu-toogle");
 const nav = document.querySelector(".nav-items");
 const nameError = document.getElementById("name-error");
 const phoneError = document.getElementById("phone-error");
 const emailError = document.getElementById("email-error");
 const messageError = document.getElementById("message-error");
 const submitError = document.getElementById("submit-error");
-let icon = document.querySelector(".fa-moon");
-let btn = document.querySelector(".btn-dark");
-let h1 = document.querySelector(".heading");
-let sub_heading = document.querySelector(".sub-heading");
-let text_fe = document.querySelector(".text-fe");
-let heading_skills = document.querySelector(".heading-skills");
-let heading_contact = document.querySelector(".heading-contact");
-let text_contact = document.querySelector(".text-contact");
+const contactMessage = document.getElementById("contact-message");
+const contactEmail = document.getElementById("contact-email");
+const contactPhone = document.getElementById("contact-phone");
+const contactName = document.getElementById("contact-name");
+const icon = document.querySelector(".fa-moon");
+const btn = document.querySelector(".btn-dark");
+const h1 = document.querySelector(".heading");
+const sub_heading = document.querySelector(".sub-heading");
+const text_fe = document.querySelector(".text-fe");
+const heading_skills = document.querySelector(".heading-skills");
+const heading_contact = document.querySelector(".heading-contact");
+const text_contact = document.querySelector(".text-contact");
 
 // dark mode
 btn.onclick = function () {
@@ -31,23 +35,22 @@ btn.onclick = function () {
 };
 
 // hamburger
-menuToggle.addEventListener("click", function () {
+document.body.onclick = function () {
+  nav.classList.toggle("active");
+};
+
+menuToggle.addEventListener("click", function (e) {
+  e.stopPropagation();
   nav.classList.toggle("active");
 });
 
-let anchor = document.querySelectorAll(".nav-items li a");
-for (let i = 0; i < anchor.length; i++) {
-  anchor[i].addEventListener("click", function () {
-    let active = nav.classList.contains("active");
-    if (active) {
-      nav.classList.remove("active");
-    }
-  });
-}
+window.addEventListener("click", function () {
+  nav.classList.remove("active");
+});
 
 // validate form
-function validateName() {
-  const name = document.getElementById("contact-name").value;
+const validateName = contactName.addEventListener("input", function () {
+  const name = this.value;
 
   if (name.length == 0) {
     nameError.innerHTML = "Name is required";
@@ -59,10 +62,10 @@ function validateName() {
   }
   nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
   return true;
-}
+});
 
-function validatePhone() {
-  const phone = document.getElementById("contact-phone").value;
+const validatePhone = contactPhone.addEventListener("input", function () {
+  const phone = this.value;
 
   if (phone.length == 0) {
     phoneError.innerHTML = "Phone no is required";
@@ -78,10 +81,10 @@ function validatePhone() {
   }
   phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
   return true;
-}
+});
 
-function validateEmail() {
-  const email = document.getElementById("contact-email").value;
+const validateEmail = contactEmail.addEventListener("input", function () {
+  const email = this.value;
 
   if (email.length == 0) {
     emailError.innerHTML = "Email no is required";
@@ -98,34 +101,100 @@ function validateEmail() {
   }
   emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
   return true;
-}
+});
 
-function validateMessage() {
-  const message = document.getElementById("contact-message").value;
+const validateMessage = contactMessage.addEventListener("input", function () {
+  const message = this.value;
   const required = 30;
   const left = required - message.length;
-
+  console.log("test");
   if (left > 0) {
     messageError.innerHTML = left + " more characters required";
     return false;
   }
-
   messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
   return true;
-}
+});
 
 function validateForm() {
-  if (
-    !validateName() ||
-    !validatePhone() ||
-    !validateEmail() ||
-    !validateMessage()
-  ) {
+  if (!validateName || !validatePhone || !validateEmail || !validateMessage) {
     submitError.style.display = "block";
     submitError.innerHTML = "Please fix error to send message";
     setTimeout(function () {
       submitError.style.display = "none";
     }, 3000);
     return false;
+  }
+}
+
+// Function untuk tampilin element pas di scroll
+
+window.addEventListener("scroll", muncul);
+function muncul() {
+  let tinggiLayar = window.innerHeight;
+  let ukuranScroll = 150;
+
+  hilangg(".about", "tampil-atas");
+  hilangg(".skills", "tampil-samping");
+  hilangg(".content-about", "tampil-samping");
+  hilangg(".portofolio", "tampil-atas");
+  hilangg(".contact", "tampil-samping");
+  hilangg(".contact", "tampil-samping");
+  setTimeout(function () {
+    hilangg(".logo1", "tampil-samping");
+  }, 1000);
+  setTimeout(function () {
+    hilangg(".logo2", "tampil-samping");
+  }, 1300);
+  setTimeout(function () {
+    hilangg(".logo3", "tampil-samping");
+  }, 1600);
+  setTimeout(function () {
+    hilangg(".logo4", "tampil-samping");
+  }, 1900);
+  setTimeout(function () {
+    hilangg(".logo5", "tampil-samping");
+  }, 2200);
+  setTimeout(function () {
+    hilangg(".logo6", "tampil-samping");
+  }, 2500);
+  setTimeout(function () {
+    hilangg(".logo7", "tampil-samping");
+  }, 2800);
+  setTimeout(function () {
+    hilangg(".logo8", "tampil-samping");
+  }, 3100);
+
+  // ------------------------------------------------------------
+
+  function hilangg(element, arah) {
+    let elements = document.querySelector(element);
+    let jarakAtas1 = elements.getBoundingClientRect().top;
+
+    if (jarakAtas1 < tinggiLayar - ukuranScroll) {
+      elements.classList.add(arah);
+    } else {
+      elements.classList.remove(arah);
+    }
+  }
+}
+
+// supaya Scroll nya mulus
+
+var posisiY = 0;
+var jarak = 20;
+function smoothScroll(id) {
+  var target = document.getElementById(id).offsetTop;
+  var scrollAnimate = setTimeout(function () {
+    smoothScroll(id);
+  }, 5);
+
+  posisiY = posisiY + jarak;
+
+  if (posisiY >= target) {
+    clearTimeout(scrollAnimate);
+    posisiY = 0;
+  } else {
+    window.scroll(0, posisiY);
   }
 }
